3d0ccaad42544d198e1477a29792045ef0336772
omanbuluatie <oman.buluatie@gmail.com>
2025-05-23 15:57:18 +0900

    update css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Web - Dev3709ers</title>
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style_main.css') }}">
     
 </head>
 
